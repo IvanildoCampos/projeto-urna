b62bbe09c06b90a155ab4db477dc83087be7efa5
Ivanildo Campos <125228040+IvanildoCampos@users.noreply.github.com>
2023-11-30 22:03:46 -0300

    atualização

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'Governador',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '88888',
+                nome: 'ivanildo1',
+                partido: 'ABC',
+                fotos:[
+                    {url:'Imagem.jpeg', legenda: 'Governador'}
+                ]
+            },
+            {
+                numero: '99999',
+                nome: 'ivanildo2',
+                partido: 'CDE',
+                fotos: [
+                    {url:'imagem2.jpeg', legenda:'Governador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Presidente',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '88',
+                nome: 'ivanildo3',
+                partido: 'FGH',
+                vice: 'ivanildo4',
+                fotos:[
+                    {url:'imagem3.jpeg', legenda: 'Presidente'},
+                    {url:'imagem4.jpeg', legenda:'vice-presidente', small: true}
+                ]
+            },
+            {
+                numero: '99',
+                nome: 'ivanildo5',
+                partido:'IJKL',
+                vice:'ivanildo6',
+                fotos:[
+                    {url:'imagem5.jpeg', legenda: 'Presidente'},
+                    {url:'imagem6.jpeg', legenda: 'vice-presidente', small: true}
+                ]
+            },
+        ]
+    }
+];
